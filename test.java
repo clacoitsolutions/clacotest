f6ca95835d0a827d1383c068e4b8f34c0cfc2628
claco2024 <clacolko@gmail.com>
2024-06-01 19:53:20 +0530

    we add code for add two number

---
+++ test.java
@@ -0,0 +1,18 @@
+public class test {
+      int x = 5;
+      int y = 6;
+      int sum = x + y;
+System.out.println(sum); // Print the sum of x + y
+System.out.println(sum); // Print the sum of x + y
+
+      int x = 5;
+      int y = 6;
+      int sum = x + y;
+System.out.println(sum); // Print the sum of x + y
+System.out.println(sum); // Print the sum of x + y
+      int x = 5;
+      int y = 6;
+      int sum = x + y;
+System.out.println(sum); // Print the sum of x + y
+System.out.println(sum); // Print the sum of x + y
+}
